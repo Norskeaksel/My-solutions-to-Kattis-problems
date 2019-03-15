ed3c647991a1120079935db2bcfbf7892d1ac7cf
Aksel <ahsorbye@gmail.com>
2019-03-15 23:49:21 +0100

    Add solution

---
+++ Herman.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main() {
+	long double radius;
+	cin >> radius;
+	cout << acos(-1)*radius*radius << endl;
+	cout << radius * radius * 2 << endl;
+}
