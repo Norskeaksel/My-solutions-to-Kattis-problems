10471dbad662575ee78cc0679de60b6d541df141
Aksel <ahsorbye@gmail.com>
2019-04-08 11:16:00 +0200

    Add solution

---
+++ All_about_that_base.cpp
@@ -0,0 +1,29 @@
+//#include<bits/stdc++.h> includerer alt, men fungerer ikke i c++
+#include<iostream>
+#include<string>
+#include <sstream>// for ting som ostream
+#include<map>
+#include<set>
+#include<vector>
+#include<algorithm>
+#include<fstream>
+#include<cmath>
+#include <iomanip>
+#include <stdio.h>
+#include<utility>//pair<auto,auto> 
+#include <iomanip>
+#include <iterator>
+#include <queue>
+using namespace std;
+
+#define fori(n) for (int i=0;i<n;i++)
+#define forj(n) for (int j=0;j<n;j++)
+#define fork(n) for (int k=0;k<n;k++)
+#define forx(n) for (int x=1;x<=n;x++)
+#define fory(n) for (int y=1;y<=n;y++)
+#define ll long long
+#define double long double
+
+int main(){
+
+}
