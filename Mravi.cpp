b9f6267fcbf2382bc5f7db3b28224ac10dfd649f
Aksel <ahsorbye@gmail.com>
2019-04-10 14:30:55 +0200

    Add solution

--- Mravi.cpp
+++ Mravi.cpp
@@ -17,18 +17,18 @@ using namespace std;
 #define fori(n) for (int i=0;i<n;i++)
 #define forj(n) for (int j=0;j<n;j++)
 #define fork(n) for (int k=0;k<n;k++)
-struct vertex{
+struct vertex {
 	long double flow;
 	long double leaf;
 	int spes;
-	int parent=-2;
+	int parent = -2;
 };
-long double findFlow(vertex node, long double &current, vertex nodes[1234]) {//hvorfor &
+long double findFlow(vertex node, long double &current, vertex nodes[1234]) {
 	int par = node.parent;
 	if (par != -2) {
 		//cout << par << endl;
-		if (node.spes == 1&& current>1) {
-			current = sqrt(current)/(node.flow / 100);
+		if (node.spes == 1 && current > 1) {
+			current = sqrt(current)/ (node.flow / 100);
 		}
 		else {
 			current /= (node.flow / 100);
@@ -38,21 +38,21 @@ long double findFlow(vertex node, long double &current, vertex nodes[1234]) {//h
 	return current;
 }
 int main() {
+	vertex nodes[1234];
 	int n;
 	cin >> n;
-	vertex nodes[1234];
-	int a, b, x, t;
+	long double a, b, x, t;
 	//int biggestNode = 0;
-	fori(n-1) {
-		cin>> a >> b >> x >> t;
+	fori(n - 1) {
+		cin >> a >> b >> x >> t;
 		int slutt = max(a, b);
 		int start = min(a, b);
-		nodes[slutt-1].parent = start-1;
-		nodes[slutt-1].flow = x;
-		nodes[slutt-1].spes = t;
+		nodes[slutt - 1].parent = start -	 1;
+		nodes[slutt - 1].flow = x;
+		nodes[slutt - 1].spes = t;
 	}
 	long double in;
-	long double svar=0;
+	long double svar = 0;
 	//fori(n)cout << nodes[i].flow<< " ";
 	fori(n) {
 		cin >> in;
@@ -60,5 +60,5 @@ int main() {
 		if (in != -1)
 			svar = max(svar, findFlow(nodes[i], in, nodes));
 	}
-	cout << svar;
+	cout <<setprecision(14)<< svar;
 }
