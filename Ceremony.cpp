23c6373b995078241437371f6baaf3906a0de7e1
Aksel <ahsorbye@gmail.com>
2019-04-12 22:15:25 +0200

    Add solution

---
+++ Ceremony.cpp
@@ -0,0 +1,40 @@
+#if defined _MSC_VER //if using Visual Studio
+#include "std_lib_facilities.h" //include this local header file
+#include <queue>
+#else
+#include<bits/stdc++.h> //include this non portable headerfile 
+using namespace std; //and the standard namespace
+#endif
+#pragma warning(disable:4996)//necessary if we want to use freopen() 
+
+#define ll long long
+#define double long double
+#define dx first
+#define dy second
+#define forx(n) for (int x=1;x<=n;x++)
+#define fory(n) for (int y=1;y<=n;y++)
+#define rall(v) (v).rbegin(),(v).rend()
+#define all(v) (v).begin(),(v).end()
+#define fork(n) for (int k=0;k<n;k++)
+#define forj(n) for (int j=0;j<n;j++)
+#define fori(n) for (int i=0;i<n;i++)
+
+int main() {
+#if defined _MSC_VER 
+	//freopen("Text.txt", "r", stdin);//read all input form this local file 
+#endif
+	int t;
+	cin >> t;
+	vector<int>b;
+	int m = t;
+	fori(t){
+		int a;
+		cin >> a;
+		b.push_back(a);
+	}
+	sort(rall(b));
+	fori(t){
+		m = min(b[i]+i, m);
+	}
+	cout << m;
+}
