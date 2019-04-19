073cdcde137011f12bd6bae6e00171166d5a7c16
Aksel <ahsorbye@gmail.com>
2019-04-20 00:10:09 +0200

    Add solution

---
+++ Sum_of_the_Others.cpp
@@ -0,0 +1,52 @@
+//#include<bits/stdc++.h> includerer alt, men fungerer ikke i visual studio
+//priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
+#include<iostream>
+#include<string>
+#include <sstream>// for ting som ostream
+#include<map>
+#include<set>
+#include<vector>
+#include<algorithm>
+#include<fstream>//For å jobbe med filer
+#include<cmath>
+#include <iomanip>
+#include <stdio.h>
+#include<utility>//pair<auto,auto> 
+#include <cstdlib>//rand()
+#include <ctime> //srand()
+#include <iterator>
+#include <queue>
+#include <stack>
+#include <functional>// tillater bruk av greater<int>
+#include <stdio.h>// tillater automatisk lesing av testinnput
+using namespace std;
+
+#define fori(n) for (int i=0;i<n;i++)
+#define forj(n) for (int j=0;j<n;j++)
+#define fork(n) for (int k=0;k<n;k++)
+#define forx(n) for (int x=1;x<=n;x++)
+#define fory(n) for (int y=1;y<=n;y++)
+#define all(v) (v).begin(),(v).end()
+#define ll long long
+#pragma warning(disable:4996)
+
+vector<string> strsplit(string text) {
+	istringstream iss(text);
+	vector<string> results((istream_iterator<string>(iss)),
+		istream_iterator<string>());
+	return results;
+}
+int main() {
+#if defined _MSC_VER
+	freopen("Text.txt", "r", stdin);
+#endif
+	string line;
+	while (getline(cin,line)) {
+		int svar = 0;
+		vector<string>num=strsplit(line);
+		fori(num.size()) {
+			svar += stoi(num[i]);
+		}
+		cout << svar/2 << endl;
+	}
+}
