346f2543942e503bddf32b4412cf73e812446a5d
Aksel <ahsorbye@gmail.com>
2019-04-23 17:48:34 +0200

    Add solution

---
+++ Volim.cpp
@@ -0,0 +1,55 @@
+//#include<bits/stdc++.h> includerer alt, men fungerer ikke i visual studio
+//priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>pq;
+#include<iostream>
+#include<string>
+#include <sstream>// for ting som ostream
+#include<map>
+#include<set>
+#include<vector>
+#include<algorithm>
+#include<fstream>//For å jobbe med filer
+#include<cmath>
+#include <iomanip>
+#include <stdio.h>
+#include<utility>//pair<auto,auto> 
+#include <cstdlib>//rand()
+#include <ctime> //srand()
+#include <iterator>
+#include <queue>
+#include <stack>
+#include <functional>// tillater bruk av greater<int>
+#include <stdio.h>// tillater automatisk lesing av testinnput
+using namespace std;
+
+#define fori(n) for (int i=0;i<n;i++)
+#define forj(n) for (int j=0;j<n;j++)
+#define fork(n) for (int k=0;k<n;k++)
+#define forx(n) for (int x=1;x<=n;x++)
+#define fory(n) for (int y=1;y<=n;y++)
+#define all(v) (v).begin(),(v).end()
+#define ll long long
+#pragma warning(disable:4996)
+
+int main() {
+#if defined _MSC_VER
+	//freopen("Text.txt", "r", stdin);
+#endif
+	int lab,n,t=210;
+	char z;
+	cin >> lab >> n;
+	fori(n) {
+		int a;
+		cin >> a>>z;
+		t -= a;
+		if (t <= 0) {
+			cout << lab << endl;
+			exit(0);
+		}
+		if (z == 'T') {
+			lab++;
+			if (lab==9) {
+				lab = 1;
+			}
+		}
+	}
+}
