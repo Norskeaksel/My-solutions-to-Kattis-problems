eb55065ba14fa7abc0d8c46d4b5d8dec768f54ce
Aksel <ahsorbye@gmail.com>
2019-08-09 15:30:58 +0200

    Add solution

---
+++ Cracking_RSA.cpp
@@ -0,0 +1,50 @@
+#if defined _MSC_VER //if using Visual Studio
+#include "std_lib_facilities.h" //include this local header file
+#include <queue>
+#else
+#include<bits/stdc++.h> //include this non portable headerfile 
+using namespace std; //and the standard namespace
+#endif
+#pragma warning(disable:4996)//necessary if we want to use freopen() 
+
+#define ll long long
+#define double long double
+#define dx first
+#define dy second
+#define forx(n) for (int x=1;x<=n;x++)
+#define fory(n) for (int y=1;y<=n;y++)
+#define rall(v) (v).rbegin(),(v).rend()
+#define all(v) (v).begin(),(v).end()
+#define fork(n) for (int k=0;k<n;k++)
+#define forj(n) for (int j=0;j<n;j++)
+#define fori(n) for (int i=0;i<n;i++)
+
+int main() {
+#if defined _MSC_VER 
+	freopen("Text.txt", "r", stdin);//read all input form this local file 
+#endif
+	double pr[] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547, 557, 563, 569, 571, 577, 587, 593, 599, 601, 607, 613, 617, 619, 631, 641, 643, 647, 653, 659, 661, 673, 677, 683, 691, 701, 709, 719, 727, 733, 739, 743, 751, 757, 761, 769, 773, 787, 797, 809, 811, 821, 823, 827, 829, 839, 853, 857, 859, 863, 877, 881, 883, 887, 907, 911, 919, 929, 937, 941, 947, 953, 967, 971, 977, 983, 991, 997};
+	auto prSize = size(pr);
+	int t;
+	cin >> t;
+	while(t--){
+		double n, e;
+		cin >> n >> e;
+		double p, q;
+		fori(prSize) {
+			p = pr[i];
+			forj(prSize) {
+				q = pr[j];
+				if (p*q==n)
+					goto theEnd;
+			}
+		}
+	theEnd:
+		double phiN = (p - 1)*(q - 1);
+		double k = 1;
+		while ((k*phiN + 1) / e != floor((k*phiN + 1) / e))
+			k++;
+		int d = (k*phiN + 1) / e;
+		cout << d << endl;
+	}
+}
