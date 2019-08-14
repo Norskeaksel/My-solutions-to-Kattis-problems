39b98a8892c77057c3ac07e929255459b5ecdf1b
Aksel <ahsorbye@gmail.com>
2019-08-14 14:31:28 +0200

    Add solution

---
+++ Bridging_Signals.cpp
@@ -0,0 +1,42 @@
+#if defined _MSC_VER //if using Visual Studio
+#include "std_lib_facilities.h" //include this local header file
+#include <queue>
+#else
+#include<bits/stdc++.h> //include this non portable headerfile 
+using namespace std; //and the standard namespace
+#endif
+#pragma warning(disable:4996)//necessary if we want to use freopen() 
+
+#define ll long long
+#define double long double
+#define dx first
+#define dy second
+#define forx(n) for (int x=1;x<=n;x++)
+#define fory(n) for (int y=1;y<=n;y++)
+#define rall(v) (v).rbegin(),(v).rend()
+#define all(v) (v).begin(),(v).end()
+#define fork(n) for (int k=0;k<n;k++)
+#define forj(n) for (int j=0;j<n;j++)
+#define fori(n) for (int i=0;i<n;i++)
+
+int main() {
+ios::sync_with_stdio(false); cin.tie(0);
+#if defined _MSC_VER 
+	freopen("Text.txt", "r", stdin);//read all input form this local file 
+#endif
+	int t;
+	cin >> t;
+	vector<int>save;
+	fori(t) {
+		int a;
+		cin >> a;
+		int index = upper_bound(all(save), a)-save.begin();
+		if(index==save.size()){
+			save.push_back(a);	
+		}
+		else{
+			save[index] = a;
+		}
+	}
+	cout << save.size() << endl;
+}
