875880910ac0782ddfbd9c37cc49a0e2181b89f1
Aksel <ahsorbye@gmail.com>
2019-08-15 21:03:43 +0200

    Add solution

--- Iforaneye.cpp
+++ Iforaneye.cpp
@@ -7,49 +7,38 @@ using namespace std; //and the standard namespace
 #endif
 #pragma warning(disable:4996)//necessary if we want to use freopen() 
 
-#define ll long long
-#define double long double
+using ll = long long;
+using ld = long double;
+using vll = vector<ll>;
+using vld = vector<ld>;
+using pll = pair<ll, ll>;
+
 #define dx first
 #define dy second
-#define forx(n) for (int x=1;x<=n;x++)
-#define fory(n) for (int y=1;y<=n;y++)
+#define forx(n) for (ll x=1;x<=n;x++)
+#define fory(n) for (ll y=1;y<=n;y++)
 #define rall(v) (v).rbegin(),(v).rend()
 #define all(v) (v).begin(),(v).end()
-#define fork(n) for (int k=0;k<n;k++)
-#define forj(n) for (int j=0;j<n;j++)
-#define fori(n) for (int i=0;i<n;i++)
+#define fork(n) for (ll k=0;k<n;k++)
+#define forj(n) for (ll j=0;j<n;j++)
+#define fori(n) for (ll i=0;i<n;i++)
+#define in(t) ll t; cin >> t; while(t--)
+#define fin(n) ll n; cin >> n; fori(n)
+#define trav(v) for(auto &e:v)
+#define sz(x) (ll)(x).size()
+#define pb(x) push_back(x)
 
-void findAndReplaceAll(string & data, string toSearch, string replaceStr)
-{
-	// Get the first occurrence
-	size_t pos = data.find(toSearch);
-
-	// Repeat till end is reached
-	while (pos != string::npos)
-	{
-		// Replace this occurrence of Sub String
-		data.replace(pos, toSearch.size(), replaceStr);
-		// Get the next occurrence from the current position
-		pos = data.find(toSearch, pos + toSearch.size());
-	}
+template <class T>
+void ipb(vector<T>&v) {
+	T a;
+	cin >> a;
+	v.pb(a);
 }
-vector<string> a = { "four", "and", "are", "bea", "bee", "eye", "for", "one", "owe", "sea", "see", "too", "two", "why", "won", "you", "at", "be", "oh", "to", "Four", "And", "Are", "Bea", "Bee", "Eye", "For", "One", "Owe", "Sea", "See", "Too", "Two", "Why", "Won", "You", "At", "Be", "Oh", "To" };
-vector<string> b = { "4", "&", "r", "b", "b", "i", "4", "1", "o", "c", "c", "2", "2", "y", "1", "u", "@", "b", "o", "2", "4", "&", "R", "B", "B", "I", "4", "1", "O", "C", "C", "2", "2", "Y", "1", "U", "@", "B", "O", "2" };
 
 int main() {
-	map<string, string>dic;
-	fori(a.size()) {
-		dic[a[i]] = b[i];
-	}
-	int t;
-	cin >> t;
-	string str;
-	getline(cin, str);
-	fori(t) {
-		getline(cin, str);
-		forj(a.size()) {
-			findAndReplaceAll(str, a[j], b[j]);
-		}
-		cout << str << endl;
-	}
+	ios::sync_with_stdio(false); cin.tie(0);
+#if defined _MSC_VER 
+	freopen("Text.txt", "r", stdin);//read all input form this local file 
+#endif
+	
 }
