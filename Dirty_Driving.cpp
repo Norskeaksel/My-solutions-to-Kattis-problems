8a7e9d39a74b101cccf01a987f6af3e42c1afcfb
Aksel <ahsorbye@gmail.com>
2019-08-18 23:00:29 +0200

    Add solution

---
+++ Dirty_Driving.cpp
@@ -0,0 +1,39 @@
+#if defined _MSC_VER //if using Visual Studio
+#include "std_lib_facilities.h" //include this local header file
+#include <queue>
+#else
+#include<bits/stdc++.h> //include this non portable headerfile 
+using namespace std; //and the standard namespace
+#endif
+#pragma warning(disable:4996)//necessary if we want to use freopen() 
+
+#define ll long long
+#define double long double
+#define dx first
+#define dy second
+#define forx(n) for (int x=1;x<=n;x++)
+#define fory(n) for (int y=1;y<=n;y++)
+#define rall(v) (v).rbegin(),(v).rend()
+#define all(v) (v).begin(),(v).end()
+#define fork(n) for (int k=0;k<n;k++)
+#define forj(n) for (int j=0;j<n;j++)
+#define fori(n) for (int i=0;i<n;i++)
+
+int main() {
+#if defined _MSC_VER 
+	freopen("Text.txt", "r", stdin);//read all input form this local file 
+#endif
+	int n, p;
+	cin >> n >> p;
+	vector<int>cd;
+	fori(n) {
+		int a;
+		cin >> a;
+		cd.push_back(a);
+	}
+	sort(all(cd));
+	int m = 0;
+	fori(n)
+		m = max(m, p*(i + 1) - (cd[i] - cd[0]));
+	cout << m;
+}
