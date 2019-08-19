a20f6db81b69b18da3b90e391107465d4d6840a9
Aksel <ahsorbye@gmail.com>
2019-08-19 14:18:41 +0200

    Add solution

--- Parking.cpp
+++ Parking.cpp
@@ -1,18 +1,43 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-using namespace std;
+#if defined _MSC_VER //if using Visual Studio
+#include "std_lib_facilities.h" //include this local header file
+#include <queue>
+#else
+#include<bits/stdc++.h> //include this non portable headerfile 
+using namespace std; //and the standard namespace
+#endif
+#pragma warning(disable:4996)//necessary if we want to use freopen() 
+
+using ll = long long;
+using ld = long double;
+#define dx first
+#define dy second
+#define forx(n) for (int x=1;x<=n;x++)
+#define fory(n) for (int y=1;y<=n;y++)
+#define rall(v) (v).rbegin(),(v).rend()
+#define all(v) (v).begin(),(v).end()
+#define fork(n) for (int k=0;k<n;k++)
+#define forj(n) for (int j=0;j<n;j++)
+#define fori(n) for (int i=0;i<n;i++)
+#define in(t) int t; cin >> t; while(t--)
+
 int main() {
-	int t,n,x;
-	cin >> t;
-	for (int i = 0; i < t; i++) {
-		cin >> n;
-		vector <int> coor;
-		for (int j = 0; j < n; j++) {
-			cin >> x;
-			coor.push_back(x);
-		}
-		sort(coor.begin(), coor.end());
-		cout << (coor[coor.size() - 1]  - coor[0]) * 2 << endl;
+	ios::sync_with_stdio(false); cin.tie(0);
+#if defined _MSC_VER 
+	freopen("Text.txt", "r", stdin);//read all input form this local file 
+#endif
+	int a, b, c, s1, s2, s3, d1, d2, d3;
+	cin >> a >> b >> c >> s1 >> d1 >> s2 >> d2 >> s3 >> d3;
+	int cost[] = { 0,a,b,c };
+	int ans = 0;
+	fori(101) {
+		int c = 0;
+		if (i > s1 && i <= d1)
+			c++;
+		if (i > s2 && i <= d2)
+			c++;
+		if (i > s3 && i <= d3)
+			c++;
+		ans += c*cost[c];
 	}
+	cout << ans;
 }
