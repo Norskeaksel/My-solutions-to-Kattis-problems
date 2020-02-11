0ef5b86bdf18d3c18c752b9f7b889454171fa458
Aksel <ahsorbye@gmail.com>
2020-02-11 10:54:12 +0100

    Add solution

---
+++ How_Many_Digits.cpp
@@ -0,0 +1,45 @@
+#if defined _MSC_VER //if using Visual Studio
+#include "std_lib_facilities.h" //include this local header file
+#else
+#include<bits/stdc++.h> //include this non portable headerfile 
+using namespace std; //and the standard namespace
+#endif
+#pragma warning(disable:4996)//necessary if we want to use freopen() 
+
+using ll = long long;
+using ld = long double;
+using vll = vector<ll>;
+using vld = vector<ld>;
+using pll = pair<ll, ll>;
+using vpl = vector<pll>;
+using vpd = vector<pair<ld, ld>>;
+using vs = vector<string>;
+
+#define dx first
+#define dy second
+#define forx(n) for (ll x=1;x<=n;x++)
+#define fory(n) for (ll y=1;y<=n;y++)
+#define rall(v) (v).rbegin(),(v).rend()
+#define all(v) (v).begin(),(v).end()
+#define fork(n) for (ll k=0;k<n;k++)
+#define forj(n) for (ll j=0;j<n;j++)
+#define fori(n) for (ll i=0;i<n;i++)
+#define in() ll t; cin >> t; while(t--)
+#define trav(v) for(auto &i:v)
+#define pb(x) push_back(x)
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0);
+#if defined _MSC_VER 
+	freopen("Text.txt", "r", stdin);//read all input form this local file 
+#endif
+	ld n;
+	ld Pi = acos(-1);
+	while (cin >> n){
+		if(n==0||n==1){
+			cout << 1<<endl;
+		}
+		else
+			cout << ll(ceil(log10(2 * Pi*n) / 2 + n * (log10(n / exp(1)))))<<endl;
+	}
+}
